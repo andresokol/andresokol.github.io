981bc9fd41a1af79fa040017b3e146e34ef7c0fc
Andrey Sokolov <sokolov-as-i@yandex.ru>
2015-02-18 09:48:06 +0300

    Update index.js

--- graph/index.js
+++ graph/index.js
@@ -110,7 +110,7 @@ var draw = function() {
 var main = function() {
 	for(var i = 0; i < glen; i++) pos.push([getX(i), getY(i)]);
 	//console.log(pos);
-	for(var i = 0; i < 1000 || draw(); i++) window.setTimeout(console.log, 500, i);
+	for(var i = 0; i < 1000 || draw(); i++) window.setTimeout(console.log.bind(console), 500, i);
 }
 
 var start = function() {
